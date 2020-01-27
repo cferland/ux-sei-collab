c6bbb08fa513340cd9ea7f90583a63523a281b34
Corey Ferland <ferlandc@gmail.com>
2020-01-27 10:06:51 -0500

    added logo to header nav

--- waverly-inn/src/components/Header.js
+++ waverly-inn/src/components/Header.js
@@ -6,6 +6,7 @@ function Header(props) {
       <nav>
         <a href="#menu">Menu</a>
         <a href="#about">Our Story</a>
+        <img src="../public/images/Logo_WaverlyInn_White_Oneline.png" />
         <a href="#hours">Hours</a>
         <a href="#location">Location</a>
       </nav>
