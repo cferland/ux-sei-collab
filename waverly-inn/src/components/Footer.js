9edadc8b7cf7fcc06c6533a4e9cb7382690d8b0e
Corey Ferland <ferlandc@gmail.com>
2020-01-27 12:05:00 -0500

    footer/header styling

--- waverly-inn/src/components/Footer.js
+++ waverly-inn/src/components/Footer.js
@@ -7,7 +7,8 @@ function Footer(props) {
         <img className="logo" src="images/Logo_WaverlyInn_White.png" alt="The Waverly Inn and Garden" />
       </div>
       <div className="copyright">
-        <p4>©2020 The Waverly Inn & Garden. All rights reserved.</p4>
+        <p4>©2020 The Waverly Inn & Garden.</p4>
+        <p4>All rights reserved.</p4>
       </div>
       <div className="contact">
         <h4>Contact Us</h4>
@@ -17,8 +18,8 @@ function Footer(props) {
       </div>
       <div className="social">
         <h4>Follow Us</h4>
-        <a target="_blank" href="https://www.facebook.com/pages/Waverly-Inn/121071064574087"><img src="/images/Icon_Facebook.svg" alt="Facebook" /></a>
-        <a target="_blank" href="https://www.instagram.com/explore/locations/387124/waverly-inn/?hl=en"><img src="/images/Icon_Instagram.svg" alt="Instagram" /></a>
+        <div className="icon"><a target="_blank" href="https://www.facebook.com/pages/Waverly-Inn/121071064574087"><img src="/images/Icon_Facebook.svg" alt="Facebook" /></a></div>
+        <div className="icon"><a target="_blank" href="https://www.instagram.com/explore/locations/387124/waverly-inn/?hl=en"><img src="/images/Icon_Instagram.svg" alt="Instagram" /></a></div>
       </div>
     </footer>
   )
