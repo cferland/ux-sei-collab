7eb0cb337c2721319d4dc5102c5df6f949b5d79f
nolanGHUB <nolanrgiles@gmail.com>
2020-01-27 12:32:18 -0500

    Added Menu data and positioning

--- waverly-inn/src/components/MenuItem.js
+++ waverly-inn/src/components/MenuItem.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
-export default MenuItem = () => {
+const MenuItem = (props) => {
   return (
-    <div>
-
+    <div className="menuitem">
+      <div className="menuitem-title-and-price">
+        <p3>{props.title}</p3>
+        <p3>{props.price}</p3>
+      </div>
+      <div>
+        <p4 className="menuitem-description">{props.description}</p4>
+      </div>
     </div>
   )
 }
+
+export default MenuItem
