c155dbee6fa2e0eb8057d72fda6aa5a6b4b7c97e
nolanGHUB <nolanrgiles@gmail.com>
2020-01-27 09:55:45 -0500

    nolan - basic boilerplate stuff

--- waverly-inn/src/App.js
+++ waverly-inn/src/App.js
@@ -4,11 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
 
-=======
-     
->>>>>>> b4eb724d81f005c3df684b97818f8dc75e3277ac
     </div>
   );
 }
--- waverly-inn/src/index.js
+++ waverly-inn/src/index.js
@@ -3,18 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-<<<<<<< HEAD
-import { BrowserRouter as Router } from 'react-router-dom'
-
-ReactDOM.render(<Router>
-  <App />
-</Router>, document.getElementById('root'));
-=======
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <Router><App /></Router>, document.getElementById('root'));
->>>>>>> b4eb724d81f005c3df684b97818f8dc75e3277ac
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
