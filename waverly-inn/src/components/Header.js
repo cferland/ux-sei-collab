08384c6567509c9e9f44c3f2003da116ea345f8d
Corey Ferland <ferlandc@gmail.com>
2020-01-27 16:17:08 -0500

    initialized hamburger menu

--- waverly-inn/src/components/Header.js
+++ waverly-inn/src/components/Header.js
@@ -6,8 +6,26 @@ class Header extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      hamburger: false
+    }
+
+    this.handleHamburger = this.handleHamburger.bind(this);
   }
 
+  handleHamburger() {
+    if (this.state.hamburger === false) {
+      document.getElementById('hamburger-icon').src = '/images/Icon_Close.svg';
+      this.setState({
+        hamburger: true
+      })
+    } else if (this.state.hamburger === true) {
+      document.getElementById('hamburger-icon').src = '/images/Icon_Hamburger.svg';
+      this.setState({
+        hamburger: false
+      })
+    }
+  }
 
   render() {
     return (
@@ -18,6 +36,7 @@ class Header extends Component {
           <img className="header-logo" src="/images/Logo_WaverlyInn_White_Oneline.png" alt="The Waverly Inn" />
           <a href="#hours"><h3>Hours</h3></a>
           <a href="#location"><h3>Location</h3></a>
+          <img id="hamburger-icon" src="/images/Icon_Hamburger.svg" onClick={this.handleHamburger} />
         </nav>
         <Carousel />
         <div id="header-button">
