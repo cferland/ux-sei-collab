bb723039b573a5592aa835de0b029bdfadead6d4
Corey Ferland <ferlandc@gmail.com>
2020-01-27 15:06:13 -0500

    added header image

--- waverly-inn/src/components/Reservation.js
+++ waverly-inn/src/components/Reservation.js
@@ -1,23 +1,23 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 
 
 class Reservation extends Component {
-    constructor(props) {
-      super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render(){
-    return(
-        <div>
-            <button className="ResButton">
-            <a href="https://www.opentable.com/restref/client/?restref=40330&lang=en&corrid=a6a3d431-a3d4-4290-b55e-fe5463858fb2">
-            <h4>Make a Reservation</h4>
-            </a>
-            </button>
-        </div>
+  render() {
+    return (
+      <div>
+        <a href="https://www.opentable.com/restref/client/?restref=40330&lang=en&corrid=a6a3d431-a3d4-4290-b55e-fe5463858fb2">
+          <button className="ResButton">
+            Make a Reservation
+          </button>
+        </a>
+      </div>
     )
-}
+  }
 }
 
 export default Reservation;
