537fbed91ef9f9bb15f8d85f3ee4556fed82658e
oelglad <omnia_elglad@aol.com>
2020-01-27 10:01:20 -0500

    initialize app

--- waverly-inn/src/App.js
+++ waverly-inn/src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
   render(){
   return (
     <div className="App">
-       <Route path="/about" render={() => <OurStory />} />
+      <OurStory />
     </div>
   );
 }
